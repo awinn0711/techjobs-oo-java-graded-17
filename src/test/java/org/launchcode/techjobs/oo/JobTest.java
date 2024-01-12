89f46a497437139b97ab115198c68f2e18cabd96
Austin Winn <awinn0711@gmail.com>
2024-01-11 19:22:32 -0600

    completed through task 4

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,36 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class JobTest {
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertNotEquals(job1.getId(), job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(testJob.getName() instanceof String);
+        assertEquals(testJob.getName(), "Product tester");
+        assertTrue(testJob.getEmployer() instanceof Employer);
+        assertEquals(testJob.getEmployer().toString(), "ACME");
+        assertTrue(testJob.getLocation() instanceof Location);
+        assertEquals(testJob.getLocation().toString(), "Desert");
+        assertTrue(testJob.getPositionType() instanceof PositionType);
+        assertEquals(testJob.getPositionType().toString(), "Quality control");
+        assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals(testJob.getCoreCompetency().toString(), "Persistence");
+    }
+    @Test
+    public void testJobsForEquality() {
+        //TODO create two job objects with identical fields except id. test for inequality
+        Job testJob1 =  new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job testJob2 =  new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertFalse(testJob1.equals(testJob2));
+    }
 }
