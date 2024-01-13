6d688bdde78ebfd2e880050e6f097f3438fed98c
Austin Winn <awinn0711@gmail.com>
2024-01-12 22:30:06 -0600

    passing all tests

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -4,17 +4,11 @@ import java.util.Objects;
 
 public class Location extends JobField {
 
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
     public Location() {
         id = nextId;
         nextId++;
     }
 
-    // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
-    //  constructor should also call the empty constructor in order to initialize the 'id' field.
     public Location(String value) {
         this();
         this.value = value;
